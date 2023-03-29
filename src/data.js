76f1c3ac356153d5b2e6bb6b69b941f304015c70
akanshsaini <120281152+akanshsaini@users.noreply.github.com>
2023-03-29 19:51:21 +0530

    Update data.js

--- src/data.js
+++ src/data.js
@@ -22,7 +22,7 @@ const data = [
   },
 ];
 
-const startTime = new Date('March 23 2022 20:42').getTime();
+const startTime = new Date('March 31 2023 11:00').getTime();
 const questionTime = 5;
 const numberOfQuestions = 3;
 
